bfcae01ae61cef5cc413260e9173d1a97721cf1b
vidit <vidit@vinsol.com>
2013-12-23 01:01:09 +0530

    conditional corrections

--- lib/field_value_validator.rb
+++ lib/field_value_validator.rb
@@ -1,13 +1,13 @@
 class FieldValueValidator < ActiveModel::Validator
   
   def validate(record)
-    if record.field.required && record.value.nil?
+    if record.field.required && record.value.blank?
       record.errors[:base] << "#{ record.field.name } cannot be empty."
     elsif record.field.unique && !record.is_value_unique?
       record.errors[:base] << "#{ record.field.name } is already taken."
-    elsif !(record.value =~ record.get_regexp) && record.value
+    elsif record.value.present? && !(record.value =~ record.get_regexp)
       record.errors[:base] << "#{ record.field.name } must be a #{ record.field.input_type }."
     end
   end
 
-end
+end	
