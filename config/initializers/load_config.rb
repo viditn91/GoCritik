81e2dcc5600bd72626829fa3271621dfb5a0a078
vidit <vidit@vinsol.com>
2013-12-23 01:01:52 +0530

    added regexp pattern

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,11 +1,11 @@
 APP_CONFIG = HashWithIndifferentAccess.new(YAML.load_file(File.expand_path('../../config.yml', __FILE__)))
 ResourceName = APP_CONFIG[:resource][:name]
 
-InputTypeHash = [ {name: 'String' , regexp: /[\w]+/ },
-{name: 'Text' , regexp: /[\w]+/ },
+InputTypeHash = [ {name: 'String' , regexp: /\A([^\t\n]{,256})\Z/ },
+{name: 'Text' , regexp: /\A[^\t\n]+\Z/ },
 {name: 'Integer' , regexp: /\A[\d]+\Z/ },
 {name: 'Float' , regexp: /(^(\d+)(\.)?(\d+)?)|(^(\d+)?(\.)(\d+))/ },
-{name: 'Boolean' , regexp: /[true|false]/ }]
+{name: 'Boolean' , regexp: /(true|false)/ }]
 
 FieldTypeArray = [['Text Field','text_field'], ['Text Area', 'text_area'], ['Select Box', 'select'],
-  ['Radio Buttons', 'radio_button_tag']]
+  ['Radio Buttons', 'radio_button']]
