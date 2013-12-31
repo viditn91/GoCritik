30711d8692b490faa7daa32facd7fb1a87721fcc
vidit <vidit@vinsol.com>
2013-12-31 16:03:34 +0530

    added likes controller and views

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,32 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!, only: :create
+  before_action :set_like, only: :destroy
+  
+  def create
+    @like = Like.new like_params
+    @like.user_id = current_user.id
+    respond_to do |format|
+      if @like.save
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @like.destroy
+        format.js 
+      end
+    end
+  end
+
+private
+  def set_like
+    like = Like.find_by(id: params[:id])
+    like ? @like = like : redirect_to(resources_path, notice: "Record not found")
+  end
+
+  def like_params
+    params.require(:like).permit(:likeable_id, :likeable_type)
+  end
+end
