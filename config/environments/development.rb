7ba6d3d8074475b62aff23f83fcefc258488dc3a
vidit <vidit@vinsol.com>
2014-01-22 17:43:07 +0530

    some changes in the mailer templates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,14 +9,15 @@ GoCritik::Application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.smtp_settings = {
     address:
     "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
     authentication: "plain",
-    user_name: "user_name",
-    password: "password",
+    user_name: "some-username@gmail.com",
+    password: "",
     enable_starttls_auto: true
   }
 
@@ -37,7 +38,6 @@ GoCritik::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   #provide the path for ImageMagick to Paperclip
   Paperclip.options[:command_path] = "/usr/local/bin/"
