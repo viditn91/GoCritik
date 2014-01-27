afdddc7d22c29e089013847474ca6982094f690b
vidit <vidit@vinsol.com>
2014-01-27 23:06:40 +0530

    added specs for User model; added fixture for picture upload

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,85 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe User do
+describe User do
+  
+  let(:user) do
+    User.create(email: 'honey2@singh.com', password: 'yoyo!')
+  end
+  
+  describe 'validations' do
+    describe 'presence' do
+      context 'email' do
+        it { should validate_presence_of(:email) }
+      end
+      context 'password' do
+        it { should validate_presence_of(:password) }
+      end
+      context 'first_name' do
+        it { should validate_presence_of(:first_name) }
+      end
+      context 'last_name' do
+        it { should validate_presence_of(:last_name) }
+      end
+    end
+    describe 'uniqueness' do
+      context 'email' do
+        it { should validate_uniqueness_of(:email).case_insensitive }
+      end
+    end
+  end
 
-#   it "is valid with vaild attributes" do
-#     User.new(:first_name => "foo", :last_name => "bar").should be_valid
-#   end
+  describe 'associations' do
+    describe 'has_one' do
+      context 'picture' do
+        it { should have_one(:picture).dependent(:destroy) }
+      end
+    end
+    describe 'has_many' do
+      context 'reviews' do
+        it { should have_many(:reviews).dependent(:destroy) }
+      end
+      context 'ratings' do
+        it { should have_many(:ratings).dependent(:destroy) }
+      end
+      context 'likes' do
+        it { should have_many(:likes).dependent(:destroy) }
+      end
+    end
+  end
 
-#   it "is not valid without a first name" do
-#     user = User.new :first_name => nil
-#     user.should_not be_valid
-#   end
+  describe 'has_profile_picture?' do
+    context 'when picture is uploaded' do
+      it do
+        user.picture.photo = File.new(Rails.root + 'spec/fixtures/images/rails.jpg')
+        expect(user.picture.photo.to_s).not_to eq('/images/default/original/missing.jpg')
+      end
+    end
+    context 'when picture is not uploaded' do
+      it do
+        user.picture.photo = nil
+        expect(user.picture.photo.to_s).to eq('/images/default/original/missing.jpg')
+      end
+    end
+  end
 
-#   it "is not valid without a last name" do
-#     user = User.new :last_name => nil
-#     user.should_not be_valid
-#   end
+  describe 'full_name' do
+    context 'when first and last name are not set by user' do
+      it do
+        expect(user.full_name).to eq('Gocritik Member')
+      end
+    end
+    context 'when first and last name are set by user' do
+      it do
+        user.first_name = 'john'
+        user.last_name = 'carter'
+        expect(user.full_name).to eq('John Carter')
+      end
+    end
+  end
 
-# end
+  after(:all) do
+    User.delete_all
+    Picture.delete_all
+  end
+
+end
