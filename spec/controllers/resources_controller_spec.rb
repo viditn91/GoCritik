e8ec09bb15df9b5a0a4bb1377c90964a93c6568b
vidit <vidit@vinsol.com>
2014-02-03 02:13:13 +0530

    added specs for resources controller

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -2,4 +2,126 @@ require 'spec_helper'
 
 describe ResourcesController do
 
+  let(:valid_attributes) {{ email: "someone@vinsol.com", password: "secret" }}
+  let(:user) { User.create! valid_attributes }
+  let(:resource) { Resource.create!(name: "some resource", description: "some description", state: true) }
+  let(:all_fields) { Field.all }
+
+  describe "GET #new" do
+    describe "if user logged in" do
+      it "loads all fields into @fields" do
+        sign_in user
+        get :new, { }
+        expect(assigns(:fields)).to eq(all_fields)
+      end
+
+      it "renders the new template" do
+        sign_in user
+        get :new, { }
+        response.should render_template("new")
+      end
+    end
+
+    # describe "if user not logged in" do
+    #   it "redirects to the login page" do
+    #     get :new, { }
+    #     response.should redirect_to(new_user_session_path)
+    #   end
+    # end
+  end
+
+  describe "POST #create" do
+    describe "if user logged in" do
+      it "loads all fields into @fields" do
+        sign_in user
+        post :create, resource: { name: "some other resource", description: "some other description" }
+        expect(assigns(:fields)).to eq(all_fields)
+      end
+
+      it "redirects to resources path if valid resource attributes" do
+        sign_in user
+        post :create, resource: { name: "some other resource", description: "some other description" }
+        response.should redirect_to(resources_path)
+      end
+
+      it "renders the new template, if resource attributes are invalid" do
+        sign_in user
+        post :create, resource: { name: "", description: "" }
+        response.should render_template("new")
+      end
+    end
+
+    # describe "if user not logged in" do
+    #   it "redirects to the login page" do
+    #     get :new, { }
+    #     response.should redirect_to(new_user_session_path)
+    #   end
+    # end
+
+  end
+
+  describe "GET #index" do
+    it "loads all fields into @fields" do
+      get :index, { }
+      expect(assigns(:fields)).to eq(all_fields)
+    end
+
+    it "loads the liquid template for keywords into @keywords_template" do
+      get :show, { :id => resource.permalink }
+      expect(assigns(:keywords_template)).to eq(Template.find_by(controller: 'resources', action: 'index', view_element: 'keywords'))
+    end
+
+    it "loads all resources into @resources when no search parameter is sent" do
+      get :index, {}
+      expect(assigns(:resources)).to eq(Resource.approved)
+    end
+
+    # it "loads all resources matching the search parameter into @resources" do
+    #   get :index, { :search => "some resource" }
+    #   expect(assigns(:resources)).to eq([resource])
+    # end
+
+  end
+
+  describe "GET #show" do
+    describe "if resource found" do
+      it "loads the resource into @resource" do
+        get :show, { :id => resource.permalink }
+        expect(assigns(:resource)).to eq(resource)
+      end
+
+      it "loads the liquid template for keywords into @keywords_template" do
+        get :show, { :id => resource.permalink }
+        expect(assigns(:keywords_template)).to eq(Template.find_by(controller: 'resources', action: 'show', view_element: 'keywords'))
+      end
+
+      it "loads the liquid template for ratings into @tags_template" do
+        get :show, { :id => resource.permalink }
+        expect(assigns(:tags_template)).to eq(Template.find_by(controller: 'resources', action: 'show', view_element: 'tags'))
+      end
+
+      it "renders the new template" do
+        get :show, { :id => resource.permalink }
+        response.should render_template("show")
+      end
+    end
+
+    describe "if resource not found" do
+      describe "if http referer exists" do
+        it "redirects to the referer" do
+          request.env['HTTP_REFERER'] = admin_path
+          get :show, { :id => 'something-wierd' }
+          response.should redirect_to(admin_path)
+        end
+      end
+      
+      describe "if http referer does not exist" do
+        it "redirects to the resources_path" do
+          request.env['HTTP_REFERER'] = nil
+          get :show, { :id => 'something-wierd' }
+          response.should redirect_to(resources_path)
+        end
+      end
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -128,20 +128,3 @@ describe UsersController do
     end
   end
 end
-    # it "responds successfully with an HTTP 200 status code" do
-    #   get :index
-    #   expect(response).to be_success
-    #   expect(response.status).to eq(200)
-    # end
-
-    # it "renders the index template" do
-    #   get :index
-    #   expect(response).to render_template("index")
-    # end
-
-    # it "loads all of the posts into @posts" do
-    #   post1, post2 = Post.create!, Post.create!
-    #   get :index
-
-    #   expect(assigns(:posts)).to match_array([post1, post2])
-    # end
