e05e68473babf7a1d4ce56c86308424cdde7c857
vidit <vidit@vinsol.com>
2014-02-17 01:07:07 +0530

    added pushing mechanism for updated ratings on resource page

--- Gemfile
+++ Gemfile
@@ -92,3 +92,5 @@ end
 group :test do
   gem 'shoulda-matchers', '2.5.0'
 end
+
+gem 'pusher'
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,3 @@
+Pusher.app_id = '66141'
+Pusher.key = '9927e1f5ae2620aa0c36'
+Pusher.secret = '475e141097384fcbfd8e'
